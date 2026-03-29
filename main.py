e2a98aed5c1b4031c4a1e70cd56a84e4ae3f17d9
Akshay Lende <akshaylende48@gmail.com>
2026-03-29 17:54:22 +0530

    Initializing loader, splitter, and embedding components

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ load_dotenv()
 
 
 
+# Importing Dependencies
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+
+
+
+# Defining ChatBot class
+class ChatBot():
+    loader = TextLoader('')  # File reference for vectorization
+    documents = loader.load()
+
+    # text splitting 
+    text_splitter = CharacterTextSplitter(chunk_size = 1000, chunk_overlap = 4)
+    docs = text_splitter.split_documents(documents)
+
+    # Embedding Generations
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+
+    
